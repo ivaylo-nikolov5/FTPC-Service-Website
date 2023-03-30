b70ce8e438a8dc2ba82ac513bf8bfd22111226a6
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-03-30 19:23:03 +0300

    Added the files for the website

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import { React, useState } from 'react'
+import "../css/Navbar.css"
+import DropMenu from './smallComponents/DropMenu';
+
+const Navbar = () => {
+    const [clicked, setClicked] = useState(false);
+
+    function changeClicked() {
+        setClicked(prev => !prev);
+    }
+
+    return (
+        <div>
+            <div className='navbarContainer'>
+                <img src="./images/logo.png" alt=""  className='logoImage'/>
+                <div className='navMenuContainer'>
+                    <button
+                        className='homeBtn'
+                    >Home</button>
+                    <button
+                        className='servicesBtn'
+                    >Services</button>
+                    <button
+                        className='productsBtn'
+                    >Products</button>
+                </div>
+                <button
+                    className='signUpBtn'
+                >Sign Up
+                </button>
+                <div 
+                    onClick={changeClicked}
+                    className="burgerMenu"
+                >
+                    {clicked ?  <i className="fa-solid fa-x"></i> : <i className="fa-solid fa-bars"></i>}
+                </div>
+            </div>
+
+            {clicked && <DropMenu clicked={clicked}/>}
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "../css/App.css";
+import DescriptionSection from '../components/DescriptionSection.jsx';
+import Navbar from "../components/Navbar.jsx";
+import TitleSection from '../components/TitleSection.jsx';
+import SectionDelimiter from '../components/smallComponents/SectionDelimiter';
+import Footer from '../components/Footer';
+
+const MainPage = () => {
+    return (
+        <div>
+            <Navbar />
+            <TitleSection />
+            
+            <SectionDelimiter />
+
+            <DescriptionSection />
+
+            <SectionDelimiter/>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default MainPage
